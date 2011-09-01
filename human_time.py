4ed136a54c1dee469b385e5936e03a78b58498ef
Aaron Bassett <me@aaronbassett.com>
2011-09-01 12:31:59 +0100

    Fix some bugs and add noon

--- human_time.py
+++ human_time.py
@@ -1,20 +1,24 @@
 from datetime import datetime
 
-def human_time():
-    hr = int(datetime.now().hour)
-    mi = int(datetime.now().minute)
+def human_time(datetime_obj=datetime.now()):
+    
+    hr = int(datetime_obj.hour)
+    mi = int(datetime_obj.minute)
     
     hours = ["midnight", "one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine",
-            "ten", "eleven", "midnight", "one", "two", "three", "four",
+            "ten", "eleven", "noon", "one", "two", "three", "four",
             "five", "six", "seven", "eight", "nine",
-            "ten", "eleven"]
+            "ten", "eleven", "midnight"]
     
+    if mi >= 35:
+        hr = hr + 1
     
-    if mi < 4:
-        human_str = "%s o'clock" % hours[hr] if hr != 0 else hours[hr]
-    elif mi > 57:
-        human_str = "%s o'clock" % hours[(hr + 1)] if hr + 1 != 0 else hours[(hr + 1)]
+    if mi < 4 or mi > 57:
+        if hours[hr] == "noon" or hours[hr] == "midnight":
+            human_str = hours[hr]
+        else:
+            human_str = "%s o'clock" % hours[hr]
     else:
         if mi < 7:
             human_str = "five past %s" % hours[hr]
@@ -27,10 +31,10 @@ def human_time():
         elif mi < 35:
             human_str = "half past %s" % hours[hr]
         elif mi < 43:
-            human_str = "twenty to %s" % hours[(hr + 1)]
+            human_str = "twenty to %s" % hours[hr]
         elif mi < 49:
-            human_str = "quarter to %s" % hours[(hr + 1)]
+            human_str = "quarter to %s" % hours[hr]
         else:
-            human_str = "five to %s" % hours[(hr + 1)]
+            human_str = "five to %s" % hours[hr]
     
     return human_str
